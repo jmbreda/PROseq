f62f352edf35d1d797502fdf77bdc46c7fed7e09
jmbreda <jeremiebreda@disroot.org>
2023-08-23 10:56:17 +0200

    Stranded coverage

--- scripts/make_track_hubs.py
+++ scripts/make_track_hubs.py
@@ -37,52 +37,54 @@ if __name__ == '__main__':
     # make trackDb.txt
     Samples = os.listdir('results/star/')
     Samples.sort()
+    Strands = ['forward','reverse']
     outfile=f'{outfolder}/{genome}/trackDb.txt'
     with open(outfile,'w', encoding="utf-8") as fout:
-        for sample in Samples:
-            print(sample)
-            name = '_'.join(sample.split('_')[:3])
+        for strand in Strands:
+            for sample in Samples:
+                print(sample)
+                name = '_'.join(sample.split('_')[:3])
 
-            # BigWig tracks
-            fout.write(f"track {name}\n")
-            fout.write("type bigWig\n")
-            fout.write(f"bigDataUrl http://upnaesrv1.epfl.ch/PROseq/tracks/{sample}/coverage_norm.bw\n")
-            #fout.write("type bam\n")
-            #fout.write(f"bigDataUrl http://upnaesrv1.epfl.ch/PROseq/tracks/{sample}/Aligned.sortedByCoord.out.bam\n")
-            fout.write(f"shortLabel {name}\n")
-            fout.write(f"longLabel {sample}\n")
-            fout.write("visibility full\n")
-            fout.write("autoScale on\n")
-            fout.write(f"\n")
+                # BigWig tracks
+                fout.write(f"track {name} {strand}\n")
+                fout.write("type bigWig\n")
+                fout.write(f"bigDataUrl http://upnaesrv1.epfl.ch/PROseq/tracks/{sample}/NormCoverage_{strand}.bw\n")
+                #fout.write("type bam\n")
+                #fout.write(f"bigDataUrl http://upnaesrv1.epfl.ch/PROseq/tracks/{sample}/Aligned.sortedByCoord.out.bam\n")
+                fout.write(f"shortLabel {name} {strand}\n")
+                fout.write(f"longLabel {sample} {strand}\n")
+                fout.write("visibility full\n")
+                fout.write("autoScale on\n")
+                fout.write(f"\n")
 
-            # BAM tracks
-            # Optional settings
-            #
-            # pairEndsByName  any value                   # presence indicates paired-end alignments
-            # pairSearchRange N                           # max distance between paired alignments, default 20,000 bases
-            # bamColorMode    strand|gray|tag|off         # coloring method, default is strand
-            # bamGrayMode     aliQual|baseQual|unpaired   # grayscale metric, default is aliQual
-            # bamColorTag     XX                          # optional tag for RGB color, default is "YC"
-            # minAliQual      N                           # display only items with alignment quality at least N, default 0
-            # showNames       on|off                      # if off, don't display query names, default is on
-            #
-            # name            track label                 # default is "User Track"
-            # description     center label                # default is "User Supplied Track"
-            # visibility      squish|pack|full|dense|hide # default is hide (will also take numeric values 4|3|2|1|0)
-            # bigDataUrl      https://your.bam.bai.com    # default is the bigDataUrl with .bai added to the end
-            # priority        N                           # default is 100
-            # db              genome database             # e.g. hg18 for Human Mar. 2006
-            # maxWindowToDraw N                           # don't display track when viewing more than N bases
-            # chromosomes     chr1,chr2,...               # track contains data only on listed reference assembly sequences
-            # doWiggle        on|off                      # if on, show data as density graph, default is off
-#
-            #fout.write(f"track {name}\n")
-            #fout.write("type bam\n")
-            #fout.write(f"bigDataUrl http://upnaesrv1.epfl.ch/PROseq/tracks/{sample}/Aligned.sortedByCoord.out.bam\n")
-            ##fout.write(f"name={name} ")
-            #fout.write(f"description={sample} ")
-            #fout.write("visibility=full ")
-            #fout.write(f"db={genome} ")
-            #fout.write("chromosomes=chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,ch11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chrX,chrY ")
-            #fout.write("autoScale on\n")
-            #fout.write("\n")
+                # BAM tracks
+                # Optional settings
+                #
+                # pairEndsByName  any value                   # presence indicates paired-end alignments
+                # pairSearchRange N                           # max distance between paired alignments, default 20,000 bases
+                # bamColorMode    strand|gray|tag|off         # coloring method, default is strand
+                # bamGrayMode     aliQual|baseQual|unpaired   # grayscale metric, default is aliQual
+                # bamColorTag     XX                          # optional tag for RGB color, default is "YC"
+                # minAliQual      N                           # display only items with alignment quality at least N, default 0
+                # showNames       on|off                      # if off, don't display query names, default is on
+                #
+                # name            track label                 # default is "User Track"
+                # description     center label                # default is "User Supplied Track"
+                # visibility      squish|pack|full|dense|hide # default is hide (will also take numeric values 4|3|2|1|0)
+                # bigDataUrl      https://your.bam.bai.com    # default is the bigDataUrl with .bai added to the end
+                # priority        N                           # default is 100
+                # db              genome database             # e.g. hg18 for Human Mar. 2006
+                # maxWindowToDraw N                           # don't display track when viewing more than N bases
+                # chromosomes     chr1,chr2,...               # track contains data only on listed reference assembly sequences
+                # doWiggle        on|off                      # if on, show data as density graph, default is off
+    #
+                #fout.write(f"track {name}\n")
+                #fout.write("type bam\n")
+                #fout.write(f"bigDataUrl http://upnaesrv1.epfl.ch/PROseq/tracks/{sample}/Aligned.sortedByCoord.out.bam\n")
+                ##fout.write(f"name={name} ")
+                #fout.write(f"description={sample} ")
+                #fout.write("visibility=full ")
+                #fout.write(f"db={genome} ")
+                #fout.write("chromosomes=chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,ch11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chrX,chrY ")
+                #fout.write("autoScale on\n")
+                #fout.write("\n")
