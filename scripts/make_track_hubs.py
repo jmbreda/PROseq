4a9fe19e8f92039cd4796d740d3a4d54e61aac56
jmbreda <jeremiebreda@disroot.org>
2023-08-25 15:03:03 +0200

    multiwig tracks

--- scripts/make_track_hubs.py
+++ scripts/make_track_hubs.py
@@ -4,71 +4,88 @@ if __name__ == '__main__':
 
     genome = 'mm39'
     bin_size = 100
-    target = '/data/web/sites/PROseq/'
-    link = 'track_hubs'
-    os.system(f'ln -s {target} {link}')
+    track_hub_folder = '/data/web/sites/PROseq_0_4_44h'
+    track_hub_url = 'http://upnaesrv1.epfl.ch/PROseq_0_4_44h'
+
+    #link tracks to track_hub folder
+    link_to_data = '/data/web/sites/PROseq_0_4_44h/tracks'
+    data_folder = '/bigdata/jbreda/PROseq/results/binned_norm_counts/'
+    if not os.path.exists(link_to_data):
+        os.system(f'ln -s {data_folder} {link_to_data}')
 
     # make track hub
-    outfile=f'{link}/hub.txt'
+    outfile=f'{track_hub_folder}/hub.txt'
     with open(outfile,'w', encoding="utf-8") as fout:
         fout.write("hub PRO-seq\n")
         fout.write("shortLabel PRO-seq\n")
-        fout.write("longLabel PRO-seq data in mouse at times 0h-44h in steps of 4h\n")
+        fout.write("longLabel PRO-seq data in mouse at times 0h-44h in steps of 4h, forward and reverse stand\n")
         fout.write("genomesFile genomes.txt\n")
         fout.write("email jeremie.breda@epfl.ch\n")
         fout.write("descriptionUrl PROseq.html\n")
         fout.write("\n")
 
     # make genomes.txt
-    outfile=f'{link}/genomes.txt'
+    outfile=f'{track_hub_folder}/genomes.txt'
     with open(outfile,'w', encoding="utf-8") as fout:
         fout.write(f"genome {genome}\n")
         fout.write(f"trackDb {genome}/trackDb.txt\n")
         fout.write("\n")
 
     # make ChIP_Atlas.html
-    outfile=f'{link}/PROseq.html'
+    outfile=f'{track_hub_folder}/PROseq.html'
     with open(outfile,'w', encoding="utf-8") as fout:
         fout.write("PRO-seq data in mouse at times 0h-44h in steps of 4h\n")
 
     # make url.txt
-    outfile=f'{link}/url.txt'
+    outfile=f'{track_hub_folder}/url.txt'
     with open(outfile,'w', encoding="utf-8") as fout:
-        fout.write("http://upnaesrv1.epfl.ch/PROseq/hub.txt\n")
+        fout.write(f"{track_hub_url}/hub.txt\n")
 
     # make trackDb.txt
-    Samples = [f'PRO_SEQ_CT{4*i:02d}_S{i+1}_R1_001' for i in range(12)]    
+    Samples = [f'PRO_SEQ_CT{4*i:02d}_S{i+1}_R1_001' for i in range(12)]
     Strands = ['forward','reverse']
 
-    os.makedirs(f"{link}/{genome}", exist_ok=True)
-    outfile=f"{link}/{genome}/trackDb.txt"
+    os.makedirs(f"{track_hub_folder}/{genome}", exist_ok=True)
+    outfile=f"{track_hub_folder}/{genome}/trackDb.txt"
     with open(outfile,'w', encoding="utf-8") as fout:
-        for strand in Strands:
-            for sample in Samples:
-                name = '_'.join(sample.split('_')[:3])
+        for sample in Samples:
+            name = '_'.join(sample.split('_')[:3])
+            time = int(sample.split('_')[2][2:])
 
-                # BigWig tracks
-                fout.write(f"track {name}_{strand}\n")
-                fout.write("type bigWig\n")
-                fout.write(f"bigDataUrl http://upnaesrv1.epfl.ch/PROseq/tracks/{sample}/NormCoverage_3p_{strand}_bin{bin_size}bp.bw\n")
-                #fout.write("type bam\n")
-                #fout.write(f"bigDataUrl http://upnaesrv1.epfl.ch/PROseq/tracks/{sample}/Aligned.sortedByCoord.out.bam\n")
+            # BigWig composite tracks
+            fout.write(f"track {name}\n")
+            fout.write("type bigWig\n")
+            fout.write("container multiWig\n")
+            fout.write(f"shortLabel t={time}h\n")
+            fout.write(f"longLabel {sample}\n")
+            fout.write("visibility full\n")
+            fout.write("aggregate solidOverlay\n")
+            fout.write("showSubtrackColorOnUi on\n")
+            #fout.write("maxHeightPixels 500:100:8\n")
+            fout.write("autoScale on\n")
+            fout.write(f"html {name}.html\n")
+            fout.write("\n")
+            
+            for strand in Strands:
+
+                fout.write(f"\ttrack {name}_{strand}\n")
+                fout.write(f"\tshortLabel {time}_{strand[0]}\n")
+                fout.write(f"\tlongLabel {sample}_{strand}\n")
+                fout.write(f"\tbigDataUrl {track_hub_url}/tracks/{sample}/NormCoverage_3p_{strand}_bin{bin_size}bp.bw\n")
+                fout.write(f"\tparent {name}\n")
+                fout.write("\ttype bigWig\n")
                 if strand == 'forward':
-                    fout.write("color 0,0,255\n")
-                    fout.write(f"shortLabel {name}_F\n")
+                    fout.write("\tcolor 0,0,255\n")
                 else:
-                    fout.write("color 255,0,0\n")
-                    fout.write(f"shortLabel {name}_R\n")
-                fout.write(f"longLabel {sample}_{strand}\n")
-                fout.write("visibility full\n")
-                fout.write("autoScale on\n")
-                fout.write(f"html {name}_{strand}.html\n")
+                    fout.write("\tcolor 255,0,0\n")
+                    fout.write("\tnegateValues on\n")
                 fout.write(f"\n")
 
-                # make description file
-                outfile=f'{link}/{genome}/{name}_{strand}.html'
-                with open(outfile,'w', encoding="utf-8") as fout2:
-                    fout2.write(f"{sample}_{strand}\nTime point: {name.split('_')[2][2:]}h\nStrand: {strand}\n")
+        
+            # make description file
+            outfile=f'{track_hub_folder}/{genome}/{name}.html'
+            with open(outfile,'w', encoding="utf-8") as fout2:
+                fout2.write(f"{sample}_{strand}\nTime point: {name.split('_')[2][2:]}h\nTop: forward stand\nBottom: reverse strand\n")
 
 
                 # BAM tracks
