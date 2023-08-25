e73caf3f5a5e55baa7202eaaf156f1657273c295
jmbreda <jeremiebreda@disroot.org>
2023-08-25 12:03:41 +0200

    workflow outputs in different folders

--- scripts/upload_to_upnae.sh
+++ scripts/upload_to_upnae.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
 # This script is used to upload the results of the pipeline to the UPNAE
-
-Samples=$(ls -d results/star/PRO_SEQ_CT*_S*_R1_001)
-
-#echo "Enter passphrase for key '/home/jbreda/.ssh/id_rsa':"
-#read -rs password
-
-for s in $Samples
-do
-    echo "Uploading $s"
-    #sshpass -p "${password}" scp "$s"/*.bedgraph "$s"/*.bw "$s"/*.txt upnae:PROseq/"$s"
-    #scp "$s"/*.bedgraph "$s"/*.bw "$s"/*.txt upnae:PROseq/"$s"
-    scp "$s"/NormCoverage_3p_*_bin100bp.bw upnae:PROseq/"$s"
-done
+#scp -r results/star upnae:PROseq/results/
+#scp -r results/counts upnae:PROseq/results/
+#scp -r results/norm_counts upnae:PROseq/results/
+scp -r results/binned_norm_counts upnae:PROseq/results/
