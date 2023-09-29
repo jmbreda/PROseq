5b4db3a5e1d943a191bcaca0c2a6efd5a362b85e
jmbreda <jeremiebreda@disroot.org>
2023-09-29 16:33:47 +0200

    plot space time fourier

---
+++ scripts/plot_time_space_fourier_transform.py
@@ -0,0 +1,60 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Plot time-space fourier transform')
+    parser.add_argument('--table_amp', help='Input table with amplitude', type=str)
+    parser.add_argument('--outfig', help='Output figure', type=str)
+    parser.add_argument('--chr', help='Chromosome', type=str)
+    parser.add_argument('--strand', help='Strandness', type=str)
+    parser.add_argument('--bin_size', help='Bin size', default=100, type=int)
+
+    return parser.parse_args()
+
+# main
+if __name__ == '__main__':
+    args = parse_args()
+
+    a_xk = pd.read_csv(f"{args.table_amp}",sep=',',index_col=0)
+
+    # fill missing values
+    # add empty rows for missing positions
+    if True:
+        pos = a_xk.index.astype(int)
+        idx_miss = np.where( (pos[1:] - pos[:-1]) > 1000 )[0]
+        new_pos = (pos[idx_miss] + pos[idx_miss+1])//2
+        #new_pos = np.sort( list( set(np.arange(pos[0],pos[-1]+args.bin_size,args.bin_size,dtype='int')) - set(pos) ) )
+        a = np.zeros((len(new_pos),a_xk.shape[1]))
+        #a[:] = np.nan
+        a = pd.DataFrame(a,index=new_pos,columns=a_xk.columns)
+        a_xk = pd.concat([a_xk,a],axis=0)
+        a_xk.sort_index(inplace=True)
+
+    Lambda = np.array(a_xk.columns,dtype='float')
+    K = 2*np.pi/Lambda
+    Pos = np.array(a_xk.index,dtype='float')
+
+    # plot
+    fig, axes = plt.subplots(1, 2,figsize=(25,5),gridspec_kw=dict(width_ratios=[7,1]))
+
+    # plot amplitude as a function of position and k
+    ax = axes[0]
+    ax.imshow(np.array(a_xk).T,aspect='auto',cmap='jet')#extent=[Pos[0], Pos[-1], K[0], K[-1]],origin='lower')
+
+    idx = np.unique(np.linspace(0,len(Pos)-1,20).astype(int))
+    ax.xaxis.set_ticks(idx)
+    ax.xaxis.set_ticklabels(np.round(Pos[idx]/1e6,3))
+    ax.set_xlabel(f"{args.chr} ({args.strand}) position [Mb]")
+
+    ax.yaxis.set_ticks(np.arange(0,len(Lambda),5))
+    ax.yaxis.set_ticklabels(np.round(Lambda[::5]/1e3))
+    ax.set_ylabel(r"$\lambda [kb]$")
+
+    # plot sum amplitude as a function of k
+    ax = axes[1]
+    ax.plot(np.nanmean(a_xk,0),range(len(Lambda)))
+    ax.axis('off')
+
+    plt.savefig(args.outfig)
--- scripts/time_space_Fourrier_transform.py
+++ scripts/time_space_Fourrier_transform.py
@@ -4,13 +4,61 @@ import matplotlib.pyplot as plt
 import pyBigWig as bw
 import os
 from scipy.stats import beta
+import argparse
 
-def Waves_amplitude_wavelength(chr,strand,f,omega,T,K,bin_size,win_bp):
+def parse_args():
+    parser = argparse.ArgumentParser(description='Get time-space fourier transform')
+    parser.add_argument('--bin_size', help='Bin size', default=100, type=int)
+    parser.add_argument('--bw_folder', help='Input data folder', type=str)
+    parser.add_argument('--out_table_amp', type=str)
+    parser.add_argument('--out_table_phase', type=str)
+    parser.add_argument('--out_table_mu', type=str)
+    parser.add_argument('--chr', help='Chromosome', type=str)
+    parser.add_argument('--strand', help='Strandness', type=str)
+    
+    return parser.parse_args()
+
+def main():
+
+    args = parse_args()
+
+    # Time (time points, period and angular frequency)
+    T = np.arange(0,48,4) # hours
+    n = 1 # harmonic
+    P = 24 # hours
+    omega = 2*np.pi*n/P # rad per hour
+
+    # Space (wave length and wave numbers)
+    win_bp = 1_000_000 # bp
+    sigma = 2/5*win_bp # bp
+    N_lambda = 51 # number of wave lengths
+    l_min = 10*args.bin_size # bp
+    l_max = 10_000_000
+    Lambda = np.logspace(np.log10(l_min),np.log10(l_max),int((N_lambda-1)/2))
+    Lambda = np.concatenate([Lambda,[np.inf],-np.flip(Lambda)])
+    K = 2*np.pi/Lambda
+
+    # load bigwig files
+    bw_files = {}
+    for i,t in enumerate(T):
+        sample = f'PRO_SEQ_CT{t:02d}_S{i+1}_R1_001'
+        bw_files[t] = bw.open(f"{args.bw_folder}/{sample}/NormCoverage_3p_{args.strand}_bin{args.bin_size}bp.bw")
+
+    # get amplitude and phase
+    a_xk, phi_xk, mu_x = Waves_amplitude_wavelength(args.chr,bw_files,omega,T,Lambda,K,sigma,args.bin_size,win_bp)
+
+    # write to file
+    a_xk.to_csv(args.out_table_amp)
+    phi_xk.to_csv(args.out_table_phase)
+    mu_x.to_csv(args.out_table_mu)
+
+
+def Waves_amplitude_wavelength(chr,bw_files,omega,T,Lambda,K,sigma,bin_size,win_bp):
 
     # get data
     df = pd.DataFrame(columns=['start','end'])
     for t in T:
-        df_t = pd.DataFrame(f[t][strand].intervals(chr))
+        df_t = pd.DataFrame(bw_files[t].intervals(chr))
         df_t.columns = ['start','end',f"{t}"]
         df = pd.merge(df,df_t,on=['start','end'],how='outer')
     df.sort_values('start',inplace=True)
@@ -18,18 +66,9 @@ def Waves_amplitude_wavelength(chr,strand,f,omega,T,K,bin_size,win_bp):
     # remove position with 75% or more missing values (at least 5 out of 12 time points)
     df = df.loc[df.isna().sum(1)/len(T) < 0.75,:]
     df.reset_index(inplace=True,drop=True)
-    
-    # fill missing values
-    if False:
-        starts = np.sort( list( set(np.arange(df.start.values[0],df.end.values[-1],bin_size,dtype='int')) - set(df.start.values) ) )
-        ends = starts + bin_size
-        df_missing = pd.DataFrame({'start':starts,'end':ends})
-        df = pd.concat([df,df_missing],axis=0)
-        df.sort_values('start',inplace=True)
-        df.reset_index(inplace=True,drop=True)
 
     # log transform and add pseudo counts and sum for gene expression
-    Pos = (df.start.values + df.end.values)/2
+    Pos = ( (df.start.values + df.end.values)/2 ).astype(int)
     X = np.log(df[[str(t) for t in T]].values + 1/bin_size)
     del df
 
@@ -45,123 +84,45 @@ def Waves_amplitude_wavelength(chr,strand,f,omega,T,K,bin_size,win_bp):
     N_x_win = int( 2*(win_bp/bin_size) + 1 )
 
     # make X_pktx
-    X_pktx = np.zeros((N_gen_pos,1,N_time,N_x_win),dtype=complex)
-    X_pktx[:] = np.nan
-    for i in range(N_gen_pos):
-        x = Pos[i]
-        idx = np.where((Pos >= x-win_bp) & (Pos <= x+win_bp))[0]
-        idx_ = (idx - i) +  int(win_bp/bin_size)
-        X_pktx[i,0,:,idx_] = X[idx,:]
+    #X_pktx = np.zeros((N_gen_pos,1,N_time,N_x_win),dtype=complex)
+    #X_pktx[:] = np.nan
+    #for i in range(N_gen_pos):
+    #    x = Pos[i]
+    #    idx = np.where((Pos >= x-win_bp) & (Pos <= x+win_bp))[0]
+    #    idx_ = (idx - i) +  int(win_bp/bin_size)
+    #    X_pktx[i,0,:,idx_] = X[idx,:]
 
     x = np.arange(-win_bp,win_bp+bin_size,bin_size)
     w = np.exp(-x**2/(2*sigma**2))
     # spatio-temporal fourier transform
     # Dimentions: (genomic positions, wave numbers, time dim, space dim)
-    f_xk = np.sum(np.nansum( X_pktx*w[None,None,None,:]*np.exp(-1j*(omega*T[None,None,:,None] - K[None,:,None,None]*x[None,None,None,:])),3),2)
+    f_xk = np.zeros((N_gen_pos,N_wave_numbers),dtype=complex)
+    mu_x = np.zeros((N_gen_pos,1))
+    for i in range(N_gen_pos):
+        print(f"{i}/{N_gen_pos}")
+
+        idx = np.where((Pos >= Pos[i]-win_bp) & (Pos <= Pos[i]+win_bp))[0]
+        idx_ = (idx - i) +  int(win_bp/bin_size)
+
+        X_ktx = np.zeros((1,N_time,N_x_win),dtype=complex)
+        X_ktx[:] = np.nan
+        X_ktx[0,:,idx_] = X[idx,:]
+
+        f_xk[i,:] = np.sum(np.nansum( X_ktx*w[None,None,:]*np.exp(-1j*(omega*T[None,:,None] - K[:,None,None]*x[None,None,:])),2),1)
+
+        mu_x[i] = 1/N_time * np.sum(np.nansum(X_ktx*w,2),1)
+
 
     # amplitude and phase
     a_xk = 4/N_time * np.abs(f_xk) # *4 ??
-    #phi_xk = np.arctan2(np.imag(f_k),np.real(f_k)) # ?? -im/re ??
-    #mu_xk = 1/N * np.sum(np.sum(X_b*w,2),1)
+    phi_xk = np.arctan2(np.imag(f_xk),np.real(f_xk))
 
-    # plot amplitude as a function of position and k
-    fig, axes = plt.subplots(1, 2, width_ratios=[5, 1],figsize=(25,5))
+    a_xk = pd.DataFrame(a_xk,columns=Lambda,index=Pos)
+    phi_xk = pd.DataFrame(phi_xk,columns=Lambda,index=Pos)
+    mu_x = pd.DataFrame(mu_x,columns=['mu'],index=Pos)
 
-    ax = axes[0]
-    ax.imshow(a_xk.T,aspect='auto',cmap='jet',interpolation='none')
-    ax.set_xlabel(f"{chr} ({strand}) position [Mb]")
-    ax.set_ylabel(f"lambda")
-    ax.xaxis.set_ticks(np.arange(0,len(Pos),1000))
-    ax.xaxis.set_ticklabels(np.round(Pos[::1000]/1e6))
+    return a_xk, phi_xk, mu_x
 
-    ax.yaxis.set_ticks(np.arange(0,len(Lambda),5))
-    ax.yaxis.set_ticklabels(np.round(Lambda[::5]))
 
-    # plot sum amplitude as a function of k
-    ax = axes[1]
-    ax.plot(np.mean(a_xk,0),range(len(Lambda)))
-    ax.axis('off')
-
-    plt.savefig(f"results/fig/space_time_Fourier/Bins_{bin_size}bp/{chr}_{strand}_amp_k.pdf")
-
-    return a_xk 
-
-# main
 if __name__ == '__main__':
-
-    # Parameters
-    Samples = [f'PRO_SEQ_CT{4*i:02d}_S{i+1}_R1_001' for i in range(12)]
-    CHR = [f'chr{i}' for i in range(1,20)] + ['chrX','chrY','chrM']
-    Strands = ['+','-']
-
-    # Time (time points, period and angular frequency)
-    T = np.arange(0,48,4) # hours
-    n = 1 # harmonic
-    P = 24 # hours
-    omega = 2*np.pi*n/P # rad per hour
-
-    # Space (wave length and wave numbers)
-    bin_size = 10000 # bp
-    win_bp = 1_000_000 # bp
-    #WIN = [1000,3000,10000] 
-    #win = WIN[0]
-    sigma = 2/5*win_bp # bp
-    N_lambda = 51 # number of wave lengths
-    l_min = 10*bin_size # bp
-    l_max = 10_000_000
-    Lambda = np.logspace(np.log10(l_min),np.log10(l_max),int((N_lambda-1)/2))
-    Lambda = np.concatenate([Lambda,[np.inf],-np.flip(Lambda)])
-
-    # get wave number range (in bins)
-    K = 2*np.pi/Lambda
-
-    # output plot folder
-    os.makedirs(f'results/fig/space_time_Fourier/Bins_{bin_size}bp/',exist_ok=True)
-
-    # load bigwig files
-    bw_folder = 'results/binned_norm_counts'
-    f = {}
-    for sample in Samples:
-        t = int(sample.split('_')[2][2:])
-        f[t] = {}
-        for strand in Strands:
-            if strand=='+':
-                fin = f"{bw_folder}/{sample}/NormCoverage_3p_forward_bin{bin_size}bp.bw"
-            elif strand=='-':
-                fin = f"{bw_folder}/{sample}/NormCoverage_3p_reverse_bin{bin_size}bp.bw"
-            f[t][strand] = bw.open(fin)
-
-    # loop over chromosomes and strands
-    for chr in CHR:
-        print(chr)
-        for strand in Strands:
-            print(strand)
-            # compute wave amplitude and phase
-            
-            a_xk = Waves_amplitude_wavelength(chr,strand,f,omega,T,K,bin_size,win_bp)
-
-            # compute fit's R2 and p-value
-            #x_hat = mu_k[:,None] + 0.5 * a_k[:,None] * np.cos(2 * np.pi / P * T[None,:] + phi_k[:,None])
-            #sig2_res = np.var(X - x_hat,1)
-            #sig2_tot = np.var(X,1)
-            #R2 = np.zeros(sig2_res.shape)
-            #R2[sig2_tot==0] = 0
-            #R2[sig2_tot!=0] = 1 - sig2_res[sig2_tot!=0] / sig2_tot[sig2_tot!=0]
-            #p = 3
-            #pval = 1 - beta.cdf(R2, (p - 1) / 2, (N - p) / 2)
-            #phi_n[phi_n<0] += np.pi * 2
-
-            # phase and amplitude
-            #df = df.loc[:,['start','end']]
-            #df['chr'] = chr
-            #df['strand'] = strand
-            #df['phase'] = phi_n
-            #df['amplitude'] = a_n
-            #df['R2'] = R2
-            #df['pval'] = pval
-            #df['mean_log_expression'] = X.mean(1)
-
-            ## reorder columns
-            #df = df[['chr','start','end','strand','phase','amplitude','R2','pval','mean_log_expression']]
-
-            #df_out = pd.concat([df_out,df],ignore_index=True)
+    main()
