Fix loglikelihood Kalman on gene

scripts/KalmanFilter.py
@@ -61,20 +61,16 @@ class KalmanFilter(object):
 
     def update(self, z, t=None):
 
-        if np.isnan(z).all():
-            self.μ = self.μ_pred
-            self.Σ = self.Σ_pred
+        self.residual = z - self.H @ self.μ_pred # residual
+        if t is None:
+            self.S = self.H @ self.Σ_pred @ self.H.T + self.R # residual covariance
         else:
-            self.residual = z - self.H @ self.μ_pred # residual
-            if t is None:
-                self.S = self.H @ self.Σ_pred @ self.H.T + self.R # residual covariance
-            else:
-                self.S = self.H @ self.Σ_pred @ self.H.T + self.R[t] # residual covariance
-            K = self.Σ_pred @ self.H.T @ np.linalg.inv(self.S) # Kalman gain matrix
-            self.μ = self.μ_pred + K @ self.residual # updated (a posteriori) state estimate
-            I = np.eye(self.n) # n-size identity matrix
-            self.Σ = (I - K @ self.H) @ self.Σ_pred # updated (a posteriori) estimate covariance
-        
+            self.S = self.H @ self.Σ_pred @ self.H.T + self.R[t] # residual covariance
+        K = self.Σ_pred @ self.H.T @ np.linalg.inv(self.S) # Kalman gain matrix
+        self.μ = self.μ_pred + K @ self.residual # updated (a posteriori) state estimate
+        I = np.eye(self.n) # n-size identity matrix
+        self.Σ = (I - K @ self.H) @ self.Σ_pred # updated (a posteriori) estimate covariance
+    
         return self.μ, self.Σ
 
     def log_likelihood(self):
@@ -86,14 +82,27 @@ class KalmanFilter(object):
     def fullForward(self, X, u = np.array([0])):
         tables = []
         loglik = 0
+        n = 0
         # (re)-initialization
         self.μ, self.Σ = self.μ_0, self.Σ_0
         for t,z in enumerate(X.T):
+
             self.predict(t,u=u)
-            self.update(z,t)
+            
+            if np.isnan(z).all():
+                self.μ = self.μ_pred
+                self.Σ = self.Σ_pred
+            else:
+                self.update(z,t)
+                loglik += self.log_likelihood()
+                n += 1
+            
+            # Append pred and update state to table
             table=(self.μ_pred, self.Σ_pred, self.μ, self.Σ)
             tables.append(table)
-            loglik += self.log_likelihood()
+
+        # average log-likelihood over expressed space points
+        loglik /= n
         return (tables, loglik)
     
     @staticmethod

scripts/Kalman_filter_on_genes.py
@@ -6,7 +6,6 @@ from multiprocessing import Pool
 from functools import partial
 import h5py
 
-
 import sys
 sys.path.insert(0, 'scripts')
 from KalmanFilter import KalmanFilter
@@ -22,11 +21,10 @@ def parse_args():
     
     return parser.parse_args()
 
-def get_data(bw_folder,bin_size):
+def get_all_tables(bw_folder,bin_size):
     
     # Parameters
     CHR = [f'chr{i+1}' for i in range(19)] + ['chrX','chrY','chrM']
-    bin_size = 1000 # bp
     Strands = ['+', '-']
     T = np.arange(0,48,4)
 
@@ -46,6 +44,38 @@ def get_data(bw_folder,bin_size):
             # replace missing values with 0, add pseudocount, take the log
             df[chr][strand].fillna(0,inplace=True)
             df[chr][strand] = df[chr][strand].apply(lambda x: np.log(x+1/bin_size),axis=1)
+
+    return df
+
+def get_data(coord, bw_folder, bin_size):
+
+    T = np.arange(0,48,4)
+    strand_dict = {'+': 'forward', '-': 'reverse'}
+    [chr,start,end,strand] = coord.split(':')
+
+    # Load bigWigs
+    bw_files = {}
+    for t in T:
+        sample = f'PRO_SEQ_CT{t:02d}_S{t//4+1}_R1_001'
+        fin = f"{bw_folder}/{sample}/NormCoverage_3p_{strand_dict[strand]}_bin{bin_size}bp.bw"
+        bw_files[t] = bw.open(fin)
+
+    # get data
+    df = pd.DataFrame(columns=['start','end'])
+    for t in T:
+        df_t = pd.DataFrame(bw_files[t].intervals(chr,int(start),int(end)+bin_size),columns=['start','end',f"{t}"])
+        df = pd.merge(df,df_t,on=['start','end'],how='outer')
+    df.sort_values('start',inplace=True)
+    df.reset_index(inplace=True,drop=True)
+
+    # replace start and end with position in the middle of the bin, and set as index
+    df['start'] = ( (df.start.values + df.end.values)/2 ).astype(int) # bp
+    df.drop('end',axis=1,inplace=True)
+    df.columns = ['pos'] + df.columns[1:].tolist()
+    df.set_index('pos',inplace=True)
+
+    df.fillna(0,inplace=True)
+    df = df.apply(lambda x: np.log(x+1/bin_size),axis=1)
 
     return df
 
@@ -93,28 +123,37 @@ def run_kalman_with_k(X,H,R,dx,Rotate,k):
     F[1,1] = np.cos(θ)
 
     # forward noise
+    
+    sigma = dx*1e-5
     if Rotate:
         Q = np.zeros((n,n))
-        Q[0,0] = (10*θ)**2 # σ_r ^2
-        Q[1,1] = (θ/20)**2 # σ_φ ^2
+        Q[0,0] = (10*sigma)**2 # σ_r ^2
+        Q[1,1] = (sigma/20)**2 # σ_φ ^2
     else:
-        Q = np.eye(n)*(θ/10)**2
+        Q = np.eye(n)*(sigma/10)**2
 
     # initial state
     μ_0 = np.zeros(n)
     Σ_0 = np.eye(n)*1
 
+    # initialize Kalman filter class
     kf = KalmanFilter(F=F, H=H, Q=Q, R=R, μ_0=μ_0, Σ_0=Σ_0)
 
-    μ_pred = np.zeros((n,N_mes))
-    Σ_pred = np.zeros((n,n,N_mes))
-    μ_t = np.zeros((n,N_mes))
-    Σ_t = np.zeros((n,n,N_mes))
-    for i,z in enumerate(X.T):
-        μ_pred[:,i], Σ_pred[:,:,i] = kf.predict(i)
-        μ_t[:,i], Σ_t[:,:,i] = kf.update(z,i)
+    if False:
+        μ_pred = np.zeros((n,N_mes))
+        Σ_pred = np.zeros((n,n,N_mes))
+        μ_t = np.zeros((n,N_mes))
+        Σ_t = np.zeros((n,n,N_mes))
+        # Get Kalman filter forward predictions and updates
+        for i,z in enumerate(X.T):
+            μ_pred[:,i], Σ_pred[:,:,i] = kf.predict(i)
+            if np.isnan(z).all():
+                μ_t[:,i] = μ_pred[:,i]
+                Σ_t[:,:,i] = Σ_pred[:,:,i]
+            else:
+                μ_t[:,i], Σ_t[:,:,i] = kf.update(z,i)
 
-    #test Forward-Backward
+    # run Forward saving all predicted and updated stated in forward table. Also return log-likelihood (summed over space and time)
     forward_table, ll = kf.fullForward(X)
 
     μ_tT, Σ_tT = kf.Backward(forward_table,F)
@@ -124,8 +163,6 @@ def run_kalman_with_k(X,H,R,dx,Rotate,k):
     # save the best
     return ll, μ_tT, Σ_tT
 
-
-
 if __name__ == '__main__':
     
     args = parse_args()
@@ -134,7 +171,7 @@ if __name__ == '__main__':
     gtf = get_gtf(args.gtf)
     
     # get data
-    df = get_data(args.bw_folder,args.bin_size)
+    df = get_all_tables(args.bw_folder,args.bin_size)
 
     # get noise model parametrs
     fin = open(args.noise_model_parameters,'r')
@@ -167,6 +204,9 @@ if __name__ == '__main__':
     #for gene in ["Cry1","Cry2"]:
     for gene in gtf.index:
         coord = gtf.loc[gene,['chr','start','end','strand']]
+        
+        #COORD = f"{coord.chr}:{coord.start}:{coord.end}:{coord.strand}"
+        #df = get_data(COORD, args.bw_folder, args.bin_size)
 
         # get small region of the chromosome
         idx_pos = (df[coord.chr][coord.strand].index > coord.start-args.bin_size) & (df[coord.chr][coord.strand].index < coord.end+args.bin_size)
@@ -174,16 +214,16 @@ if __name__ == '__main__':
             continue
 
         measurements = df[coord.chr][coord.strand].loc[idx_pos,:].values.T # time x position
-        positions = df[coord.chr][coord.strand].loc[idx_pos,:].index # positions
+        positions = df[coord.chr][coord.strand].loc[idx_pos,:].index # positions bins middle [bp]
 
-        # fill missing values
+        # fill missing values with nans
         x = np.arange(positions[0],positions[-1]+1,args.bin_size)
         idx = [np.where(x==pos)[0][0] for pos in positions]
         X = np.zeros((measurements.shape[0],x.shape[0]))*np.nan
         X[:,idx] = measurements
         [m,N_mes] = X.shape # number of measurements
 
-        # Use unnormalized expression at each position for R
+        # Use unnormalized expression noise model for R
         R = np.zeros((len(x),m,m))
         # exponential decay of R as a function of z :  R(x) = a * exp(-b * x) + c
         for i in range(len(x)):

scripts/plot_kalman_on_genes.py
@@ -4,10 +4,7 @@ import pyBigWig as bw
 import h5py
 import argparse
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import axes3d
-from mpl_toolkits.mplot3d import art3d
-from matplotlib.patches import Ellipse
-import matplotlib.transforms as transforms
+
 
 
 def parse_args():
@@ -16,8 +13,7 @@ def parse_args():
     parser.add_argument('--bw_folder', help='Input data folder', default="results/binned_norm_counts" ,type=str)
     parser.add_argument('--gtf', help='Gene gtf file',default="resources/genome/GRCm39/gene_protein_coding.gtf", type=str)
     parser.add_argument('--in_hdf5', default='results/output.hdf5', type=str)
-    parser.add_argument('--out_ll_plot', default='results/plot.pdf', type=str)
-    parser.add_argument('--out_r2_plot', default='results/r2_plot.pdf', type=str)
+    parser.add_argument('--out_fig', default='results/plot.pdf', type=str)
     
     return parser.parse_args()
 
@@ -25,7 +21,6 @@ def get_all_data(bw_folder,bin_size):
     
     # Parameters
     CHR = [f'chr{i+1}' for i in range(19)] + ['chrX','chrY','chrM']
-    bin_size = 1000 # bp
     Strands = ['+', '-']
     T = np.arange(0,48,4)
 
@@ -78,7 +73,7 @@ def get_data(coord, bw_folder, bin_size):
     df.set_index('pos',inplace=True)
 
     df.fillna(0,inplace=True)
-    df = df[chr][strand].apply(lambda x: np.log(x+1/bin_size),axis=1)
+    df = df.apply(lambda x: np.log(x+1/bin_size),axis=1)
 
     return df
 
@@ -119,85 +114,65 @@ if __name__ == '__main__':
     gtf = get_gtf(args.gtf)
 
     # Load data
-    hf = h5py.File(args.in_hdf5,'r')
+    with h5py.File(args.in_hdf5, 'r') as hf:
+        K = hf['K'][:]
+
+        Genes = list(hf.keys())
+        Genes.remove('K')
+        Genes = np.array(Genes)
+
+        LL = np.zeros((len(K),len(Genes)))
+        R2 = np.zeros(len(Genes))
+        strand = np.zeros(len(Genes),dtype=int)
+        for g, gene in enumerate(Genes):
+            LL[:,g] = hf[gene]['LL'][:]
+            X = hf[gene]['measurements'][:]
+            smoothed = hf[gene]['smoothed'][:]
+            R2[g] =  1 - np.sum( ((X.flatten() - smoothed.flatten())**2) )/np.sum( (X.flatten() - X.mean())**2 )
+            strand[g] = (1 if gtf.at[gene,'strand'] == '+' else -1)
     
-    K = hf['K'][:]
-
-    Genes = list(hf.keys())
-    Genes.remove('K')
-    LL = np.zeros((len(K),len(Genes)))
-
-    for g, gene in enumerate(Genes):
-        LL[:,g] = hf[gene]['LL'][:]
-
     LL_ratio = LL.max(0) - LL[K==0,:] # log-likelihood ratio
 
-    idx_sort = np.argsort(LL_ratio)
+    # get dataframe
+    df = gtf.copy()
+    df = df.loc[Genes,:]
+    df['Length'] = df.end - df.start
+    df['K_max'] = K[np.argmax(LL,axis=0)]
+    df['lambda_max_kb'] = (2*np.pi/df.K_max)*1e-3
+    df['LL_ratio'] = (LL.max(axis=0,keepdims=True) - LL[K==0,:])[0,:]/np.log(2)
+    df['R2'] = R2
+    df['strand'] = strand
 
+    # create figure with 3 subplots sharing the same y-axis
+    fig, axes = plt.subplots(1, 3, sharey=True, gridspec_kw={'width_ratios': [10, 1, 10], 'wspace' : 0}, figsize=(15, 8))
 
-    # plot log-likelihood
+    # plot
+    sign = np.sign(df.K_max)
+    for f, k_sign in enumerate([-1,0,1]):
+        ax = axes[f]
+        idx = (np.sign(df.K_max) == k_sign)
+        if k_sign == -1:
+            x = -df.loc[idx,'K_max']
+            ax.invert_xaxis()
+            ax.set_ylabel(r'$R^2$')
+            ax.set_xscale('log')
+        elif k_sign == 0:
+            x = df.loc[idx,'K_max']/2*np.pi*1e-3
+            ax.set_xlabel('Wavenumber (rad/kb)')
+            ax.set_xticks([0],['0'])
+        elif k_sign == 1:
+            x = df.loc[idx,'K_max']
+            ax.set_xscale('log')
 
-    lik = np.exp(LL - LL.max(0))
+        ax.scatter(x=x,y=df.loc[idx,'R2'],marker='.',s=df.loc[idx,'LL_ratio']+.5,c=df.loc[idx,'strand'],cmap='bwr_r',alpha=.5)
 
-    fig, ax = plt.subplots(figsize=(10,6))
-    h = ax.imshow(lik[:,idx_sort],cmap='jet',origin='lower')
-    ax.set_xlabel('Position [Mp]')
-    ax.set_ylabel(r'$K [rad/bp]$')
-    ax.set_title('Likelihood')
-    fig.colorbar(h, ax=ax)
-    fig.savefig(args.out_ll_plot,bbox_inches='tight')
+        if k_sign==-1:
+            x_ticklabels = ax.get_xticklabels()
+            for i in range(len(x_ticklabels)):
+                new_text = x_ticklabels[i].get_text().replace('10','-10')
+                x_ticklabels[i].set_text(new_text)
+            ax.set_xticklabels(x_ticklabels)
 
-
-
-
-    # get R2
-    T = np.arange(0,48,4) # time points [h]
-    P = 24 # period [h]
-    ω = 2*np.pi/P # angular frequency [rad/h]
-    m = len(T) # number of time points
-    n = 2 # number complex state
-    dx = args.bin_size # distance between positions
-    H = np.zeros((m,2))
-    H[:,0] = np.cos(ω*T)
-    H[:,1] = -np.sin(ω*T)
-    H /= 6
-
-    R2 = np.zeros(len(Genes))
-    for g, gene in enumerate(Genes):
-        coord = gtf.loc[gene,['chr','start','end','strand']]
-
-        coord = [coord.chr,coord.start,coord.end,coord.strand].join(':')
-
-        df = get_data(coord,args.bw_folder,args.bin_size)
-
-        # get small region of the chromosome
-        measurements = df.values.T # time x position
-        positions = df.index # positions
-
-        # fill missing values
-        x = np.arange(positions[0],positions[-1]+1,args.bin_size)
-        idx = [np.where(x==pos)[0][0] for pos in positions]
-        X = np.zeros((measurements.shape[0],x.shape[0]))*np.nan
-        X[:,idx] = measurements
-        [m,N_mes] = X.shape # number of measurements
-
-        # normalize
-        X[:,idx] -= measurements.mean(0)
-        sigma = X[:,idx].std(axis=0)
-        sigma[sigma==0] = 1
-        X[:,idx] /= sigma
-
-        smoothed = H @ hf[gene]['mu'][:].T
-        R2[g] = 1 - np.sum((hf[gene]['measurements'][:] - smoothed)**2) / np.sum((hf[gene]['measurements'][:] - hf[gene]['measurements'][:].mean(0))**2)
-    
-    fig, ax = plt.subplots(figsize=(8,6))
-    ax.hist(R2,bins=100)
-    ax.set_xlabel('R2')
-    ax.set_ylabel('Frequency')
-    ax.set_title('R2 distribution')
-    fig.savefig(args.out_r2_plot,bbox_inches='tight')
-
-
-
-
-    hf.close()
+    # save figure
+    fig.tight_layout()
+    fig.savefig(args.out_fig)