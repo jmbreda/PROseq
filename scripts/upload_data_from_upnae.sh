ec8a86aa3d1349e64379e73a07909cdf2f1b3304
jmbreda <jeremiebreda@disroot.org>
2024-04-17 11:07:45 +0200

    make envs (jed)

--- scripts/upload_data_from_upnae.sh
+++ scripts/upload_data_from_upnae.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# This script is used to upload the results of the pipeline to the UPNAE
+# This script is used to upload the data of the pipeline from and to upnae
+
+scp -r upnae:PROseq/resources/fastq resources/
 #scp -r results/star upnae:PROseq/results/
 #scp -r results/counts upnae:PROseq/results/
 #scp -r results/norm_counts upnae:PROseq/results/
-scp -r upnae:PROseq/results/binned_norm_counts/ results/
+#scp -r upnae:PROseq/results/binned_norm_counts/ results/
 #scp -r upnae:PROseq/results/phase_amp results/
 #scp -r results/kalman/Gene/* upnae:PROseq/results/kalman/Gene/
 #scp -r results/kalman/Gene_Q_1e-3/* upnae:PROseq/results/kalman/Gene_Q_1e-3/
+uxc 
